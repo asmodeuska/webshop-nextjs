24ec4a5b8876edce8475b4cc709c37b7dd33346e
asmodeuska <szaboviktor161@gmail.com>
2022-07-18 15:02:13 +0200

    firebase env

--- server/middleware/firebase.js
+++ server/middleware/firebase.js
@@ -4,7 +4,20 @@ const { mongo_connection } = require('../middleware/mongo');
 const productSchema = require('../schemas/productSchemas');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(
+    {
+      "type": "service_account",
+      "project_id": "eshop-3b7c4",
+      "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+      "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+      "client_id": process.env.FIREBASE_CLIENT_ID,
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zzpe5%40eshop-3b7c4.iam.gserviceaccount.com"
+    }
+  ),
   databaseURL: "https://eshop-3b7c4-default-rtdb.europe-west1.firebasedatabase.app"
 });
 
@@ -37,7 +50,7 @@ const login = async (req, res) => {
       const User = mongo_connection.model('users', productSchema.userSchema);
       User.findOne({ uid: uid }, (err, user) => {
         console.log(user);
-        if (user===null) {
+        if (user === null) {
           User.create({ uid: uid }, (err, user) => {
             if (err) {
               return res.status(500).send();
